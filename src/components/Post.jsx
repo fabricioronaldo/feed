c640fbeb5ef4aed537facfd8f130ea8f9a9c9277
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-05-10 21:08:03 -0300

    Estrutura da aplicação - Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,8 @@
 import styles from './Post.module.css';
 
-export function Post(){
-    
-    return(
+export function Post() {
+
+    return (
 
         <article className={styles.post}>
             <header>
@@ -15,8 +15,8 @@ export function Post(){
                 </div>
 
                 <time>Publicado há 1h</time>
-            
             </header>
+
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
@@ -26,8 +26,16 @@ export function Post(){
                     <a href="">#nlw </a>
                     <a href="">#rocketseat </a>
                 </p>
-
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Escreva um comentário...' />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
 
     );
