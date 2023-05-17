51271330b666b69a47867cc7ca2437aa6c7a2701
Fabricio Velasco <fabriciorvelasco@gmail.com>
2023-05-16 22:34:44 -0300

    Criação e estilização do componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+
+export function Comment() {
+
+    return (
+
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/fabricioronaldo.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Fabricio Ronaldo</strong>
+                            <time>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar Comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Fabricio, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir<span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+
+    )
+}
